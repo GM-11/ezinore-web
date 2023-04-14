f88faab85351a9792108780a5dd83d0127a0695d
GM-11 <gmathur1001@gmail.com>
2023-04-14 07:30:43 -0700

    added razorpay gateway

---
+++ src/routes/razorpay/+server.ts
@@ -0,0 +1,32 @@
+import type { RequestEvent } from "@sveltejs/kit";
+import { json } from "@sveltejs/kit";
+import { v4 as uuidv4 } from "uuid";
+
+import Razorpay from "razorpay";
+
+export async function POST(requestEvent: RequestEvent) {
+  const body = await requestEvent.request.json();
+
+  const { amount } = body;
+
+  const razorpay = new Razorpay({
+    key_id: process.env.RAZORPAY_KEY_ID || "",
+    key_secret: process.env.RAZORPAY_KEY_SECRET,
+  });
+
+  const payment_capture = 1;
+  const currency = "INR";
+  const options = {
+    amount: amount * 100,
+    currency,
+    receipt: uuidv4(),
+    payment_capture,
+  };
+  try {
+    const response = await razorpay.orders.create(options);
+    return json({ id: response.id, amount: response.amount }, { status: 200 });
+  } catch (error) {
+    console.log(error);
+    return json({ error: "Internal server error" }, { status: 500 });
+  }
+}
