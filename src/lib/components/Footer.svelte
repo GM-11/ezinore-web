810b62f76954f67771591f824a865fe3f7cc9b59
GM-11 <gmathur1001@gmail.com>
2023-06-01 20:44:27 +0530

    New component

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,154 @@
+<script lang="ts">
+  import logo from "$lib/assets/ezinore-logo-coloured.png";
+  import { page } from "$app/stores";
+
+  import instagram from "$lib/assets/social/instagram.png";
+  import facebook from "$lib/assets/social/facebook.png";
+  import twitter from "$lib/assets/social/twitter.png";
+  import linkedin from "$lib/assets/social/linkedin.png";
+
+  let menuOpen = false;
+  let stuffAppear = false;
+
+  function scrollToElement({ target }: { target: any }) {
+    const el = document.querySelector(target.getAttribute("href"));
+    if (!el) return;
+    stuffAppear = false;
+    menuOpen = false;
+    el.scrollIntoView({
+      behavior: "smooth",
+    });
+  }
+</script>
+
+<nav class="flex flex-col">
+  <div
+    class=" hidden z-20 text-black items-center md:flex flex-row justify-between w-full p-5"
+  >
+    {#if $page.url.pathname !== "/"}
+      <a href="/" class="nav-element"><img src={logo} width="200" alt="" /> </a>
+    {:else}
+      <a href="#home" on:click={scrollToElement}>
+        <img src={logo} width="200" alt="" class="nav-element cursor-pointer" />
+      </a>
+    {/if}
+
+    <div class="flex flex-row items-center">
+      {#if $page.url.pathname !== "/"}
+        <a href="/" class="nav-element">Home</a>
+      {:else}
+        <a
+          href="#home"
+          class="nav-element"
+          on:click|preventDefault={scrollToElement}>Home</a
+        >
+      {/if}
+      <a href="/about" class="nav-element">About</a>
+      <a href="/product" class="nav-element">Product</a>
+      <a href="/contact" class="contact">Contact Us</a>
+    </div>
+  </div>
+  <div class="flex flex-row-reverse px-3 items-center">
+    <a href="https://www.instagram.com/ezinore/" target="_blank">
+      <img src={instagram} width="30" alt="" />
+    </a>
+    <a href="https://www.facebook.com/ezinore" target="_blank">
+      <img src={facebook} width="30" alt="" />
+    </a>
+    <a href="https://twitter.com/ezinore" target="_blank">
+      <img src={twitter} width="30" alt="" />
+    </a>
+    <a href="https://www.linkedin.com/company/ezinore" target="_blank">
+      <img src={linkedin} width="30" alt="" />
+    </a>
+    <h2>Follow us:</h2>
+  </div>
+  <hr />
+  <h3>@ Ezinore 2022. All Right Reserved</h3>
+</nav>
+
+<style>
+  h3 {
+    font-style: normal;
+    font-weight: 500;
+    font-size: 15px;
+    line-height: 18px;
+    text-align: center;
+    color: #072125ad;
+    letter-spacing: -0.035em;
+    margin: 1.5rem 0 2rem 0;
+  }
+  hr {
+    border: none;
+    height: 1px;
+    background-color: #0721255e;
+    margin: 1rem;
+  }
+  h2 {
+    font-style: normal;
+    font-weight: 700;
+    font-size: 1.2rem;
+    margin-inline: 1rem;
+    color: #072125;
+  }
+  img {
+    margin-inline: 0.5rem;
+  }
+  .nav-element {
+    font-weight: 900;
+    margin-inline: 2rem;
+
+    transition: transform 0.05s ease-in;
+  }
+
+  .nav-element:hover {
+    transform: scale(1.1, 1.1);
+  }
+
+  nav {
+    background: linear-gradient(
+        0deg,
+        rgba(255, 255, 255, 0.97),
+        rgba(255, 255, 255, 0.97)
+      ),
+      #072125;
+  }
+
+  /* .nav-bar {
+      width: 0px;
+      top: 0;
+      right: 0;
+      transition: width 0.3s ease;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      position: absolute;
+      position: relative;
+      height: 100vh;
+    } */
+
+  /* .nav-bar.open {
+      width: 40%;
+    }
+  
+    .nav-element-bar {
+      font-weight: bold;
+      margin-inline: 1em;
+      transition: transform 0.05s ease-in;
+      margin-top: 1em;
+    } */
+
+  .contact {
+    margin-inline: 2rem;
+    border-radius: 30px;
+    color: black;
+    padding-top: 10px;
+    padding-bottom: 10px;
+    padding-left: 20px;
+    padding-right: 20px;
+    text-align: center;
+    justify-content: center;
+    display: flex;
+    background-color: #f4b931;
+  }
+</style>
