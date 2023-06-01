1546029a3b1eab23c264bd0d56ef11e058483256
GM-11 <gmathur1001@gmail.com>
2023-06-01 20:45:13 +0530

    added footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,7 @@
   import Navbar from "$lib/components/Navbar.svelte";
   import "../app.css";
   import { page } from "$app/stores";
+  import Footer from "$lib/components/Footer.svelte";
   // const path = $page;
 
   // if (path === "/") console.log(true);
@@ -9,15 +10,16 @@
 
 <div class="main">
   <!-- {#if $page.url.pathname !== "/"} -->
-    <Navbar/>
+  <Navbar />
   <!-- {/if} -->
   <slot />
+  <Footer />
 </div>
 
 <style>
   @import url("https://fonts.googleapis.com/css2?family=Poppins&display=swap");
   .main {
     font-family: "Poppins", sans-serif;
-    src: url('/fonts/Aspekta-100.ttf');
+    src: url("/fonts/Aspekta-100.ttf");
   }
 </style>
